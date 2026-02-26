c12debc1ec41379ad8f6902f1a70706ccb481e47
Camille <camille_daclin@orange.fr>
2026-02-26 01:37:59 +0100

    Ajout FadeSystem, porte et spawn player

--- Assets/Code/LoadSpecificScene.cs
+++ Assets/Code/LoadSpecificScene.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
     public string sceneName;
+    public Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D (Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(loadNextScene());
         }
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
     }
 }
